71a21303de791bfd950928999f2f2728cd5bf684
evildeeds <evildeedsim@gmail.com>
2021-10-14 18:20:41 +0300

    main page & to 35 countdown

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <h1>2i0.me</h1>
+
+  <app-to-35></app-to-35>
 </template>
 
 <script>
-export default {};
+import AppTo35 from './components/AppTo35.vue';
+
+export default {
+  components: {
+    AppTo35,
+  },
+};
 </script>
 
 <style>
@@ -11,7 +19,9 @@ html {
   box-sizing: border-box;
 }
 
-* {
+*,
+*::before,
+*::after {
   box-sizing: inherit;
   padding: 0;
   margin: 0;
@@ -19,5 +29,17 @@ html {
 
 body {
   font-family: sans-serif;
+  display: flex;
+  justify-content: center;
+  font-size: 10px;
+}
+
+h1 {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+
+h2 {
+  font-size: 1.5rem;
 }
 </style>
---
+++ src/components/AppTo35.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <h2>35</h2>
+    <span id="years">{{ getYears }}</span>
+    <label for="years">Years</label>
+    <span id="days">{{ getDays }}</span>
+    <label for="days">Days</label>
+    <span id="hours">{{ getHours }}</span>
+    <label for="hours">Hours</label>
+    <span id="minutes">{{ getMinutes }}</span>
+    <label for="minutes">Minutes</label>
+    <span id="seconds">{{ getSeconds }}</span>
+    <label for="seconds">Seconds</label>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      finishDate: new Date(`February 20 2024 00:00`),
+      timeToGo: 0,
+    };
+  },
+  mounted() {
+    this.getTime();
+  },
+  methods: {
+    getTime() {
+      this.setTime();
+      this.interval = setInterval(() => {
+        this.setTime();
+      }, 1000);
+    },
+    setTime() {
+      this.timeToGo = this.finishDate - new Date();
+    },
+  },
+  computed: {
+    getYears() {
+      return Math.floor(this.timeToGo / 1000 / 60 / 60 / 24 / 365);
+    },
+    getDays() {
+      return Math.floor(this.timeToGo / 1000 / 60 / 60 / 24);
+    },
+    getHours() {
+      return Math.floor(this.timeToGo / 1000 / 60 / 60);
+    },
+    getMinutes() {
+      return Math.floor(this.timeToGo / 1000 / 60);
+    },
+    getSeconds() {
+      return Math.floor(this.timeToGo / 1000);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+span {
+  display: block;
+  font-size: 1rem;
+  font-weight: 700;
+}
+
+h2 {
+  margin-bottom: 0.5rem;
+}
+</style>
