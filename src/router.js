e38065ee43c7eeb55501ce180a6da67850cb2d68
evildeeds <evildeedsim@gmail.com>
2021-10-22 08:19:20 +0300

    ...

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import Breath from './components/Breath.vue';
 import AppTo35 from './components/AppTo35.vue';
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     { path: '/', component: AppTo35 },
     { path: '/breath', component: Breath },
