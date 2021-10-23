acd648e0c4eae2fc335521f6cdab43629d4f05b8
EvilDeedsIM <evildeedsim@gmail.com>
2021-10-23 14:10:03 +0300

    pomodoro updated

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -1,13 +1,41 @@
 <template>
   <div class="time">
-    <Icon class="icon" icon="ci:chevron-duo-up" @click="addTime" />
-    <span class="minutes">{{ minutes }}</span>
+    <div class="minutes-div">
+      <Icon
+        class="icon icon-min-up"
+        icon="ci:chevron-duo-up"
+        @click="addTime('min')"
+        v-if="!pauseFlag"
+      />
+      <span class="minutes">{{ minutes }}</span>
+      <Icon
+        class="icon icon-min-down"
+        icon="ci:chevron-duo-down"
+        @click="removeTime('min')"
+        v-if="!pauseFlag"
+      />
+    </div>
     <span>:</span>
-    <span class="seconds">00</span>
-    <Icon class="icon" icon="ci:chevron-duo-down" @click="removeTime" />
+    <div class="seconds-div">
+      <Icon
+        class="icon icon-sec-up"
+        icon="ci:chevron-duo-up"
+        @click="addTime('sec')"
+        v-if="!pauseFlag"
+      />
+      <span class="seconds">{{ seconds }}</span>
+      <Icon
+        class="icon icon-sec-down"
+        icon="ci:chevron-duo-down"
+        @click="removeTime('sec')"
+        v-if="!pauseFlag"
+      />
+    </div>
   </div>
   <div class="buttons">
-    <button class="btn btn-play-pause" @click="playPause">Start</button>
+    <button class="btn btn-play-pause" @click="playPause">
+      {{ runFlag ? 'Pause' : pauseFlag ? 'Continue' : 'Start' }}
+    </button>
     <button class="btn btn-stop" @click="stop">Stop</button>
   </div>
 </template>
@@ -18,46 +46,95 @@ import { Icon } from '@iconify/vue';
 export default {
   data() {
     return {
-      minutesToGo: 1,
+      defaultMinutes: 20,
+      minutesToGo: 20,
       secondsToGo: 0,
       timeToGo: 0,
-      flag: false;
+      runFlag: false,
+      pauseFlag: false,
       interval: null,
+      oneMinute: 60,
     };
   },
   methods: {
     playPause() {
-      if (!this.flag) {
-        this.timeToGo = this.minutesToGo * 60 + this.secondsToGo;
-        this.flag = true;
+      if (!this.runFlag) {
+        this.timeToGo = this.minutesToGo * this.oneMinute + this.secondsToGo;
+        this.runFlag = true;
+        this.pauseFlag = true;
+        this.startInterval();
+        return;
       }
-      if(this.flag) {
 
-        this.flag = false;
+      clearInterval(this.interval);
+      this.runFlag = false;
+    },
+
+    stop() {
+      clearInterval(this.interval);
+      this.runFlag = false;
+      this.pauseFlag = false;
+      this.minutesToGo = this.defaultMinutes;
+      this.secondsToGo = 0;
+    },
+
+    addTime(x) {
+      if (x === 'min') {
+        if (this.minutesToGo < 60) this.minutesToGo++;
+        if (this.minutesToGo === 60) this.secondsToGo = 0;
+      }
+
+      if (x === 'sec') {
+        if (this.secondsToGo < 60 && this.minutesToGo < 60) this.secondsToGo++;
+        if (this.secondsToGo === 60 && this.minutesToGo < 60) {
+          this.secondsToGo = 0;
+          this.minutesToGo++;
+        }
       }
     },
-    stop() {},
-    addTime() {
-      if (this.timeToGo < 60) this.timeToGo++;
-    },
-    removeTime() {
-      if (this.timeToGo > 1) this.timeToGo--;
-    },
-    timeLeft() {
-      const seconds = this.timeToGo * 60;
 
-      getMinutes() {
-        return minutes;
-      };
-      getSeconds() {
-        return minutes
-      };
-    }
+    removeTime(x) {
+      if (x === 'min') {
+        if (this.minutesToGo === 1) this.secondsToGo = 0;
+        if (this.minutesToGo > 1) this.minutesToGo--;
+      }
+
+      if (x === 'sec') {
+        if (this.secondsToGo === 0 && this.minutesToGo > 1) {
+          this.secondsToGo = 59;
+          this.minutesToGo--;
+          return;
+        }
+        if (this.secondsToGo > 0) this.secondsToGo--;
+      }
+    },
+
+    reduceTimeToGo() {
+      if (this.timeToGo > 0) {
+        this.timeToGo--;
+        this.minutesToGo = Math.floor(this.timeToGo / this.oneMinute);
+        this.secondsToGo = this.timeToGo % this.oneMinute;
+        // console.log(this.timeToGo);
+        // console.log('min:', this.minutesToGo, 'sec:', this.secondsToGo);
+        return;
+      }
+
+      this.stop();
+    },
+
+    startInterval() {
+      this.interval = setInterval(this.reduceTimeToGo, 1000);
+    },
   },
   computed: {
     minutes() {
       if (this.minutesToGo < 10) return `0${this.minutesToGo}`;
       return this.minutesToGo;
+    },
+
+    seconds() {
+      if (this.secondsToGo < 10) return `0${this.secondsToGo}`;
+      return this.secondsToGo;
     },
   },
   components: {
@@ -68,10 +145,11 @@ export default {
 
 <style scoped>
 .time {
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-bottom: 2rem;
+  margin-bottom: 4rem;
 }
 
 .time span {
@@ -81,8 +159,10 @@ export default {
 }
 
 .time .icon {
+  position: absolute;
   font-size: 3rem;
   cursor: pointer;
+  margin: 0 !important;
 }
 
 .time .icon:first-child {
@@ -91,6 +171,35 @@ export default {
 
 .time .icon:last-child {
   margin-left: 0.6rem;
+}
+
+.time .minutes-div,
+.time .seconds-div {
+  position: relative;
+}
+
+.time .icon-min-up {
+  top: -36px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.time .icon-min-down {
+  bottom: -36px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.time .icon-sec-up {
+  top: -36px;
+  right: 50%;
+  transform: translateX(50%);
+}
+
+.time .icon-sec-down {
+  bottom: -36px;
+  right: 50%;
+  transform: translateX(50%);
 }
 
 .buttons {
@@ -102,11 +211,10 @@ export default {
 
 .btn {
   cursor: pointer;
-
-  width: 6rem;
+  width: 6.6rem;
   height: 2.4rem;
   text-transform: uppercase;
-  font-size: 1rem;
+  font-size: 0.8rem;
   font-weight: 600;
   font-family: Inter, sans-serif;
 }
